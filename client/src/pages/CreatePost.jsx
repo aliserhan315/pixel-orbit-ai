13ec850eedc14c1c6ee8ca177c6079ebfa3206f4
aliserhan315 <aliserhan315@gmail.com>
2024-12-13 11:37:07 +0200

    added formfield and creating page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,15 +6,6 @@ import { getRandomPrompt } from '../utils';
 import { preview } from '../assets';
 
 
-const handleSubmit =()=>{
-
-}
-const handleChange =(e)=>{
-
-}
-const handleSurpriseMe=()=>{
-  
-}
 const CreatePost = () => {
   const navigate =useNavigate();
   const [form, setForm] = useState({
@@ -23,7 +14,20 @@ const CreatePost = () => {
     photo:'',
   })
   const [generatingImg, setGeneratingImg] = useState(false)
+
   const [loading, setLoading] = useState(false)
+
+  const handleSubmit =()=>{
+
+    }
+    const handleSurpriseMe = () => {
+      const randomPrompt = getRandomPrompt(form.prompt);
+      setForm({ ...form, prompt: randomPrompt });
+    };
+const generateImage =()=>{}
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  
   return (
    <section className='max-w-7xl mx-auto'>
         <div>
@@ -37,23 +41,63 @@ const CreatePost = () => {
         <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
         <FormField
-        LableName ="Your Name"
+        labelName ="Your Name"
         type="text"
         name='name'
-        placeholder ='Adam Black'
+        placeholder ='Ali Serhan'
         value ={form.name}
         handleChange={handleChange}
         />
             <FormField
-        LableName ="prompt"
+        labelName ="prompt"
         type="text"
         name="prompt"
-        placeholder ='T-Rex battling a dragon in an epic volcanic landscape, surrounded by fire and smoke'
+        placeholder ='T-Rex battling a dragon in an epic volcanic landscape'
         value ={form.prompt}
         handleChange={handleChange}
         isSurpriseMe
         handleSurpriseMe={handleSurpriseMe}
         />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            { form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
         </div>
         </form>
 
